55c46c47835be576576ba55c1b000fc16a9b772b
Hao@Mac mini <waynehfut@outlook.com>
2024-07-22 11:54:10 +0800

    update slides

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+# define CHAP 1 //控制章节输出，在对应章节修改数字即可
+
+
 #include <stdio.h>
 #include "01intro/intro.h"
 #include "02list/list.h"
@@ -5,7 +8,13 @@
 int main() {
     int a = 10;
     int b = 12;
-    printf("recu %d\n", recu(2));
-    printf("helloworld %d\n", add(a, b));
+    switch (CHAP) {
+        case 1:
+            printf("recu %d\n", recu(2));
+            break;
+        case 2:
+            printf("helloworld %d\n", add(a, b));
+            break;
+    }
     return 0;
 }
